6916ffb4994233648a948027605f71b6d4fb9d34
kaphynia72 <jiangnaqi01@163.com>
2019-07-01 18:31:35 +0800

    添加银行卡

---
+++ pages/me/account/payPassword.vue
@@ -0,0 +1,188 @@
+<template>
+	<view class="main">
+		<block v-if="pswChange&&!pswSet&&!pswConfirm">
+			<view class="psw-back">
+				<view class="attention-text">请输入提现密码</view>
+
+				<input type="number" class="password-input" maxlength="6" :focus="true" v-model="pswSetStr"/>
+				<view class="password-item-back">
+					<block v-for="(i,index) in pswHolder" :key="index">
+						<view class="password-item">
+							<view class="password-hide-text" v-if="pswSetStr.length>index">*</view>
+						</view>
+					</block>
+				</view>
+
+				<view class="action-btn" @click="turnToPswConfirm">
+					<view class="action-btn-text">下一步</view>
+				</view>
+			</view>
+		</block>
+
+		<block v-else-if="pswSet&&!pswChange&&!pswConfirm">
+			<view class="psw-back">
+				<view class="attention-text">请设置新的提现密码</view>
+
+				<input type="number" class="password-input" maxlength="6" :focus="true" v-model="pswSetStr" />
+				<view class="password-item-back">
+					<block v-for="(i,index) in pswHolder" :key="index">
+						<view class="password-item" @click="focusSetInput">
+							<view class="password-hide-text" v-if="pswSetStr.length>index">*</view>
+						</view>
+					</block>
+				</view>
+
+				<view class="action-btn" @click="turnToPswConfirm">
+					<view class="action-btn-text">下一步</view>
+				</view>
+			</view>
+		</block>
+
+		<block v-else-if="pswConfirm&&!pswChange&&!pswSet">
+			<view class="psw-back">
+				<view class="attention-text">请再次确认提现密码</view>
+
+				<input type="number" class="password-input" maxlength="6" :focus="true" v-model="pswConfirmStr" />
+				<view class="password-item-back">
+					<block v-for="(i,index) in pswHolder" :key="index">
+						<view class="password-item" @click="focusConfirmInput">
+							<view class="password-hide-text" v-if="pswConfirmStr.length>index">*</view>
+						</view>
+					</block>
+				</view>
+
+				<view class="action-btn">
+					<view class="action-btn-text">完 成</view>
+				</view>
+			</view>
+		</block>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			checkCode: "",
+			oldPayPassword: "",
+			payPassword: "",
+			phoneNum: "",
+
+		},
+		data() {
+			return {
+				setInputFocus: false,
+				confirmInputFocus: true,
+				
+				pswSet: false,
+				pswChange: false,
+				pswConfirm: false,
+				
+				pswHolder: '012345',
+				
+				pswOld: '',
+				pswSetStr: '',
+				pswConfirmStr: ''
+			}
+		},
+		methods: {
+			focusSetInput: function() {
+				this.setInputFocus = true;
+			},
+			focusConfirmInput: function() {
+				this.confirmInputFocus = true;
+			},
+			confirmInput: function(e) {
+				console.log(e.target.value);
+			},
+			turnToPswConfirm: function() {
+				this.pswConfirm = true;
+			}
+		},
+		onLoad: function(option) {
+			console.log(option);
+			this.pswChange = option.pswChange;
+			this.pswSet = option.pswSet;
+			this.pswConfirm = option.pswConfirm;
+			console.log(option.pswChange+'/'+!option.pswChange);
+			console.log(option.pswSet+'/'+!option.pswSet);
+			console.log(option.pswConfirm+'/'+!option.pswConfirm);
+			console.log('change'+(this.pswChange&&!this.pswSet&&!this.pswConfirm));
+			console.log('set'+(!this.pswChange&&this.pswSet&&!this.pswConfirm));
+			console.log('confirm'+(!this.pswChange&&!this.pswSet&&this.pswConfirm));
+		}
+	}
+</script>
+
+<style lang="scss">
+	.main {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+
+		.psw-back {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			width: 100%;
+			height: 100%;
+			background-color: #eeeeee;
+			position: absolute;
+
+			.attention-text {
+				margin-top: 240upx;
+				color: #333333;
+				font-size: 30upx;
+			}
+
+			.password-input {
+				width: 600upx;
+				height: 90upx;
+				margin-top: 100upx;
+				color: rgba(255, 255, 255, 0);
+			}
+
+			.password-item-back {
+				z-index: 10;
+				display: flex;
+				flex-direction: row;
+				height: 90upx;
+				color: rgba(255, 255, 255, 0);
+				margin-top: -90upx;
+				background-color: #ffffff;
+				border-top: solid 1upx #E5E5E5;
+				border-left: solid 1upx #E5E5E5;
+				border-bottom: solid 1upx #E5E5E5;
+
+				.password-item {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					width: 100upx;
+					height: 90upx;
+					border-right: solid 1upx #E5E5E5;
+
+					.password-hide-text {
+						color: #333333;
+						font-size: 30upx;
+					}
+				}
+			}
+
+			.action-btn {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 80upx;
+				margin: 80upx 30upx 0upx 30upx;
+				background-color: #FF5255;
+				border: solid 1upx #FF5255;
+				border-radius: 3upx;
+
+				.action-btn-text {
+					color: #ffffff;
+					font-size: 36upx;
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/me/components/passwordModal.vue
@@ -0,0 +1,217 @@
+<template>
+	<view v-if="show" class="password-modal-back">
+		<view class="password-modal">
+			<view class="head-attention">
+				<view class="close-btn" @click="close">
+					<image class="close-btn-img"></image>
+				</view>
+				<view class="attention-text-back">
+					<view class="attention-text">请输入支付密码</view>
+				</view>
+			</view>
+
+			<view class="amount-back">
+				<view class="amout-label">提现</view>
+				<view class="amount">¥100.00</view>
+			</view>
+
+			<input type="number" class="password-input" maxlength="6" :focus="inputFocus" v-model="passwordStr" />
+			<view class="password-input-back">
+				<block v-for="(i,index) in passwordHolder" :key="index">
+					<view class="password-item">
+						<view class="password-hide-text" v-if="passwordStr.length>index">*</view>
+					</view>
+				</block>
+			</view>
+			
+			<view class="passwordForget-btn">
+				<view class="pswdForget-btn-text" @click="passwordForget">忘记密码？</view></view>
+
+			<view class="withdraw-btn">
+				<view class="withdraw-btn-text">提 现</view>
+			</view>
+		</view>
+
+	</view>
+</template>
+<script>
+	export default {
+		props: {
+			show: false,
+		},
+		data() {
+			return {
+				passwordHolder: '012345',
+				passwordStr: '',
+				inputFocus: false,
+			}
+		},
+		methods: {
+			close: function() {
+				this.$emit('close');
+			},
+			passwordForget: function() {
+				// this.close();
+				this.$emit('passwordForget');
+			},
+			focusPasswordInput: function() {
+				this.inputFocus = true
+			},
+			withdrawApplication: function() {
+				console.log(this.passwordStr);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.password-modal-back {
+		z-index: 10;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+
+	.password-modal {
+		width: 540upx;
+		height: 500upx;
+		background-color: #ffffff;
+		border: solid 0upx #ffffff;
+		border-radius: 15upx;
+	}
+
+	.head-attention {
+		display: flex;
+		flex-direction: row;
+		height: 80upx;
+		background-color: #ffffff;
+		border-top: solid 0upx #ffffff;
+		border-bottom: solid 1upx #E5E5E5;
+		border-top-left-radius: 15upx;
+		border-top-right-radius: 15upx;
+	}
+
+	.close-btn {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 80upx;
+		height: 80upx;
+		margin-left: 15upx;
+	}
+
+	.close-btn-img {
+		width: 23upx;
+		height: 23upx;
+		background-color: #007AFF;
+	}
+
+	.attention-text-back {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 350upx;
+		height: 80upx;
+	}
+
+	.attention-text {
+		color: #333333;
+		font-size: 30upx;
+		font-weight: bold;
+	}
+
+	.amount-back {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		width: 540upx;
+		height: 112upx;
+		margin-top: 30upx;
+	}
+
+	.amout-label {
+		color: #333333;
+		font-size: 28upx;
+	}
+
+	.amount {
+		color: #333333;
+		font-size: 72upx;
+		font-weight: bold;
+	}
+
+	.password-input {
+		width: 480upx;
+		height: 80upx;
+		margin-top: 30upx;
+		margin-left: 30upx;
+		color: rgba(255,255,255,0);
+	}
+
+	.password-input-back {
+		z-index: 10;
+		display: flex;
+		flex-direction: row;
+		width: 480upx;
+		height: 80upx;
+		margin-top: -80upx;
+		margin-left: 30upx;
+		border-left: solid 1upx #E5E5E5;
+		border-top: solid 1upx #E5E5E5;
+		border-bottom: solid 1upx #E5E5E5;
+	}
+
+	.password-item {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 80upx;
+		height: 80upx;
+		border-right: solid 1upx #E5E5E5;
+	}
+
+	.password-hide-text {
+		color: #333333;
+		font-size: 30upx;
+	}
+	
+	.passwordForget-btn {
+		display: flex;
+		justify-content: flex-end;
+		margin-top: 10upx;
+		margin-right: 10upx;
+	}
+	
+	.pswdForget-btn-text {
+		line-height: 35upx;
+		font-size: 28upx;
+		color: #007AFF;
+	}
+
+	.withdraw-btn {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 400upx;
+		height: 80upx;
+		margin-top: 13upx;
+		margin-left: 70upx;
+		background-color: #FF5255;
+	}
+
+	.withdraw-btn-text {
+		color: #ffffff;
+		font-size: 28upx;
+	}
+</style>
